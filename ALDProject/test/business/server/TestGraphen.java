4ef7323af194a2e31cba69c4f359aeef709a240f
sueol <sueol@172.31.33.147>
2016-06-18 11:33:11 +0200

    City.txt Test Graph

--- ALDProject/test/business/server/TestGraphen.java
+++ ALDProject/test/business/server/TestGraphen.java
@@ -22,26 +22,23 @@ public class TestGraphen {
 			String s;
 			String[] inputArray;
 			ArrayList<Street> streets = new ArrayList<Street>();
-			while((s = br.readLine())!=null)
-			{
+			while ((s = br.readLine()) != null) {
 				inputArray = s.split(";");
-				if(!(inputArray[0].equals("##"))&&inputArray.length==5)
-				{
-					streets.add(new Street(inputArray[0],Integer.parseInt(inputArray[1]),Integer.parseInt(inputArray[2]),Integer.parseInt(inputArray[3]),Integer.parseInt(inputArray[4])));
+				if (!(inputArray[0].equals("##")) && inputArray.length == 5) {
+					streets.add(
+							new Street(inputArray[0], Integer.parseInt(inputArray[1]), Integer.parseInt(inputArray[2]),
+									Integer.parseInt(inputArray[3]), Integer.parseInt(inputArray[4])));
 				}
 			}
-			
-			ListGraph listGraph = new ListGraph(streets.size(),true);
-			ArrayGraph arrayGraph = new ArrayGraph(streets.size(),true);
-			for (Street street: streets)
-			{
-				listGraph.addEdge(street.getSource_id(),street.getDestination_id(),street.getDistance());
-				arrayGraph.addEdge(street.getSource_id(),street.getDestination_id(),street.getDistance());
+
+			ListGraph listGraph = new ListGraph(streets.size(), true);
+			ArrayGraph arrayGraph = new ArrayGraph(streets.size(), true);
+			for (Street street : streets) {
+				listGraph.addEdge(street.getSource_id(), street.getDestination_id(), street.getDistance());
+				arrayGraph.addEdge(street.getSource_id(), street.getDestination_id(), street.getDistance());
 			}
-				System.out.println(listGraph.hasEdge(1,2));
-				System.out.println(arrayGraph.hasEdge(1,2));
-			
-			
+			System.out.println(listGraph.hasEdge(1, 2));
+			System.out.println(arrayGraph.hasEdge(1, 2));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
