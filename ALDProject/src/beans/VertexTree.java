8107cafbac77ac7159c7f74c4ac1c1a2807d86b8
Richard Putz <richardputz@live.de>
2016-06-21 22:51:53 +0200

    Change to Tree from Algorithmics

--- ALDProject/src/beans/VertexTree.java
+++ ALDProject/src/beans/VertexTree.java
@@ -2,44 +2,105 @@ package beans;
 
 import java.util.List;
 
-public class VertexTree {
+public abstract class VertexTree<Type> {
 
-	private City[] vertextTreeArray;
+	//private City[] vertextTreeArray;
 	private int count = 0;
 	private int index = 0 ;
 	private int capicity;
-	private Nodes root;
+	private Node<Type> root;
 	
-	public VertexTree(int size,List<City> cityList){
-		root = null;
-		capicity = size +1; 
-		vertextTreeArray = new City[capicity]; // Kapazität Baum festlegen
-		initializeTree(cityList);
-		
+	public Node<Type> getRoot() {
+		return root;
 	}
-	public void initializeTree(List<City> cityList){
-		for(City element : cityList){
-			insertCityToTree(element);
-		}		
+	public VertexTree(){
+		root = null;	
 	}
-	public void insertCityToTree(City insertCity){
-		if(index > capicity) //Next is outside of Bounds
+	/**
+	 * Neues Element hinzufügen
+	 * @param elem Hinzuzufügendes Element
+	 */
+	public void add(Type elem) {
+		Node<Type> neu = new Node<Type>(elem);
+		if (root == null) {			// Fall 1: Baum ist leer
+			root = neu;
 			return;
-		//Change to blind adding because of sorted list
-		vertextTreeArray[index] = insertCity;
-		index++;
-	}
-	private int right(int pos){
-		return (pos*2);
-	}
-	private int left(int pos){
-		return(pos*2+1);
-	}
-	//public City findBy(String searchCity,int pos){
-/*		if(vertextTreeArray[pos].getName() == searchCity){
-			return vertextTreeArray[pos];
 		}
-		else if(, o2)
-		}*/
-		//return;
+		Node<Type> node = root;				// Fall 2: Baum ist nicht leer
+		while (true) {
+			int vgl = 0;//compare(elem, node.getValue()); Add Compare
+			if (vgl < 0) {					// kleiner
+				if (node.getLeftChild() == null) {
+					node.setLeftChild(neu);
+					neu.setParent(node);
+					return;
+				}
+				node = node.getLeftChild();
+			}
+			else if (vgl > 0) {				// größer
+				if (node.getRightChild() == null) {
+					node.setRightChild(neu);
+					neu.setParent(node);
+					return;
+				}
+				node = node.getRightChild();
+			}
+			else {							// gleich (nicht nochmal einfügen)
+				return;
+			}
+		}
+	}
+
+	/**
+	 * Element im Baum finden (startet bei Root-Node)
+	 * @param needle Zu suchendes Element
+	 * @return Knoten des Elements
+	 */
+	public Node<Type> find(Type needle) {
+		return find(root, needle);
+	}
+	
+	/**
+	 * Element in Teilbaum finden (startet bei current-Node)
+	 * @param current Startknoten
+	 * @param needle  Zu suchendes Element
+	 * @return Knoten des Elements
+	 */
+	public Node<Type> find(Node<Type> current, Type needle) {
+		if (current == null) {
+			return null;
+		}
+		int vgl = 0;// ADD COMPARE Method!!!compare(needle, current.getValue());
+		if (vgl == 0) {		// Gefunden
+			return current;
+		}
+		else if (vgl < 0) {	// Links
+			return find(current.getLeftChild(), needle);
+		}
+		else {				// Rechts
+			return find(current.getRightChild(), needle);
+		}
+	}
+	
+	/**
+	 * Funktion zur Ausgabe des gesamten Baums.
+	 */
+	public void printTree() {
+		printTree(root, "");
+	}
+	
+	/**
+	 * Funktion zur Ausgabe des Baums unterhalb eines Knotens
+	 * @param current Knoten, dessen Teilbaum ausgegeben werden soll
+	 * @param prefix  Zur Einrückung
+	 */
+	public void printTree(Node<Type> current, String prefix) {
+		if (current == null) {
+			return;
+		}
+		System.out.println(prefix + current.getValue());
+		printTree(current.getLeftChild(), prefix + " L ");
+		printTree(current.getRightChild(), prefix + " R ");
+	}
+
 }
