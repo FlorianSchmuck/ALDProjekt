f10be9070c3026bba8e80f265726c01a342c2512
Richard Putz <richardputz@live.de>
2016-06-28 22:46:51 +0200

    Fix nullpointer exception for notfound citys

--- ALDProject/src/business/server/TelnetServer.java
+++ ALDProject/src/business/server/TelnetServer.java
@@ -10,13 +10,12 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import beans.ArrayGraph;
 import beans.City;
 import beans.Graph;
 import beans.ListGraph;
+import beans.Node;
 import beans.SearchCriteria;
 import beans.Street;
 import beans.VertexTree;
@@ -28,24 +27,24 @@ public class TelnetServer extends AbstractBasicServer {
 	private File cityFile, streetsFile;
 	private IOAccessLayer theIOAccessLayerInstance;
 	private List<Street> streetList;
-	private HashMap<Integer, City> cityList = new HashMap();
+	private HashMap<Integer,City> cityList = new HashMap();
 	private BufferedWriter serverCommand;
 	private boolean isServerActiv = true;
 	public static boolean isClientActiv;
 	private VertexTree vertexTree;
-	private Graph listGraph, arrayGraph;
+	private Graph listGraph,arrayGraph;
 	private GraphBrowser listGraphBrowser, arrayGraphBrowser;
 	private String path;
 	private List<Integer> flow = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
-		try {
-			new TelnetServer();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			try {
+				new TelnetServer();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
-
+	
 	public TelnetServer() throws Exception {
 		super.initializeServer();
 		initializeTelnetServer();
@@ -53,7 +52,7 @@ public class TelnetServer extends AbstractBasicServer {
 			theIOAccessLayerInstance.chooseFileFromFileSystem();
 		}
 		startServerRoutine();
-
+		
 	}
 
 	private void initializeTelnetServer() throws IOException {
@@ -64,27 +63,23 @@ public class TelnetServer extends AbstractBasicServer {
 		streetList = theIOAccessLayerInstance.readStreetsFile(streetsFile);
 		cityList = theIOAccessLayerInstance.readCityFile(cityFile);
 		addToTree();
-		listGraph = new ListGraph(cityList.size(), false); // licht
-		arrayGraph = new ArrayGraph(cityList.size(), false); // dicht
+		listGraph = new ListGraph(cityList.size(),false);	//licht
+		arrayGraph = new ArrayGraph(cityList.size(),false); //dicht
 		addToGraph(listGraph);
 		addToGraph(arrayGraph);
 	}
-
-	private void addToGraph(Graph graph) {
+	
+	private void addToGraph(Graph graph){
 		for (Street s : streetList) {
 			graph.addEdge(s.getSource_id(), s.getDestination_id(), s.getDistance());
 		}
 	}
-
-	private void addToTree() {
-		for (Entry<Integer, City> entry : cityList.entrySet()) {
-			vertexTree.add(entry.getValue().getName(),entry.getValue().getId());
+	
+	
+	private void addToTree(){
+		for(City city: cityList.values()) {
+			vertexTree.add(city.getName(), city.getId());
 		}
-
-		/*
-		 * for(City city: cityList.values()) { vertexTree.add(city.getName(),
-		 * city.getId()); }
-		 */
 	}
 
 	private void startServerShell(Socket clientSocket) throws Exception {
@@ -99,43 +94,47 @@ public class TelnetServer extends AbstractBasicServer {
 		serverCommand.newLine();
 		serverCommand.flush();
 	}
-
-	public void responseToClient(Socket clientSocket, String message) throws Exception {
-		BufferedReader clientResponse = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		String from, to, searchcriteria;
+	
+	public void responseToClient(Socket clientSocket,String message) throws Exception {
+		BufferedReader clientResponse = new BufferedReader(
+				new InputStreamReader(clientSocket.getInputStream()));
+		String from,to,searchcriteria;
 		String[] clientRequest = message.split(BasicServer.fileSeparator);
 		from = clientRequest[0];
 		to = clientRequest[1];
 		searchcriteria = clientRequest[2];
-		listGraphBrowser = new GraphBrowser(listGraph, serverCommand, cityList);
-		// arrayGraphBrowser = new GraphBrowser(arrayGraph, serverCommand);
-		System.out.println(from);
-		int fromId = vertexTree.find(from).getId();
-		int toId = vertexTree.find(to).getId();
-
+		listGraphBrowser = new GraphBrowser(listGraph, serverCommand,cityList);
+		//arrayGraphBrowser = new GraphBrowser(arrayGraph, serverCommand);
+		
+		Node<City> temp = vertexTree.find(from);
+		int fromId = searchNode(from);
+		int toId = searchNode(to);
+		if(fromId <=0 || toId <= 0)
+			return;
 		if (SearchCriteria.BREITENSUCHE.toString().equals(searchcriteria.toUpperCase())) {
 
 			listGraphBrowser.findByBreitenSuche(listGraph, fromId, toId);
-			// arrayGraphBrowser.findByBreitenSuche(arrayGraph, fromId, toId);
+			//arrayGraphBrowser.findByBreitenSuche(arrayGraph, fromId, toId);
 			path = SearchCriteria.BREITENSUCHE.toString();
 			System.out.println(path);
-		} else if (SearchCriteria.TIEFENSUCHE.toString().equals(searchcriteria.toUpperCase())) {
+		}
+		else if (SearchCriteria.TIEFENSUCHE.toString().equals(searchcriteria.toUpperCase())) 
+		{
 			listGraphBrowser.findByTiefenSucheRekursiv(listGraph, fromId, toId);
-			// arrayGraphBrowser.findByTiefenSucheRekursiv(arrayGraph, fromId,
-			// toId);
+			//arrayGraphBrowser.findByTiefenSucheRekursiv(arrayGraph, fromId, toId);
 			path = SearchCriteria.TIEFENSUCHE.toString();
 			System.out.println(path);
-		} else if (SearchCriteria.DIJKSTRA.toString().equals(searchcriteria.toUpperCase())) {
+		}
+		else if (SearchCriteria.DIJKSTRA.toString().equals(searchcriteria.toUpperCase())){
 			listGraphBrowser.dijkstraLichteGraphen(listGraph, fromId, toId);
-			// arrayGraphBrowser.dijkstraDichteGraphen(arrayGraph, fromId,
-			// toId);
+			//arrayGraphBrowser.dijkstraDichteGraphen(arrayGraph, fromId, toId);
 			path = SearchCriteria.DIJKSTRA.toString();
 			System.out.println(path);
-		}
+		}		
 		serverCommand.newLine();
 		serverCommand.flush();
 	}
-
+	
 	@Override
 	public void startServerRoutine() throws Exception {
 		while (isServerActiv == true) {
@@ -144,11 +143,22 @@ public class TelnetServer extends AbstractBasicServer {
 			WorkerThread workerThread = new WorkerThread(clientSocket, this);
 			Thread clientThread = new Thread(workerThread);
 			clientThread.start();
-
+			
 			while (isClientActiv == true) {
-				responseToClient(clientSocket, "");
+				responseToClient(clientSocket,"");
 			}
 		}
 	}
-
+	private int searchNode(String searchstring) throws IOException{
+		Node<City> temp = vertexTree.find(searchstring);
+		if(temp != null){
+			return temp.getId();
+		}
+		else{
+			serverCommand.write("Not found:" + searchstring );
+			serverCommand.flush();
+			return 0;
+		}
+	}
+	
 }
